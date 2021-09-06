26dff4798c199d5b104f9747f5546974c4d4176f
DevExpressExampleBot <fakeEmail>
2021-09-06 21:06:18 +0300

    Source auto update [skip ci]

--- VB/TreeListCheckBoxes/MainWindow.xaml.vb
+++ VB/TreeListCheckBoxes/MainWindow.xaml.vb
@@ -6,35 +6,51 @@ Imports DevExpress.Xpf.Grid
 Imports DevExpress.Xpf.Editors
 
 Namespace TreeListCheckBoxes
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
 
-    Public Class ViewModel
-        Public Property Persons() As List(Of Person)
+	Public Class ViewModel
+		Public Property Persons() As List(Of Person)
 
-        Public Sub New()
-            Persons = New List(Of Person) From { _
-                New Person With { _
-                    .IsChecked = True, .FirstName = "Billy", .LastName = "Kelm", .Children = New List(Of Person) From { _
-                        New Person With { _
-                            .IsChecked = True, .FirstName = "Daniel", .LastName = "Earwood", .Children = New List(Of Person) From { _
-                                New Person With {.IsChecked = True, .FirstName = "Anne", .LastName = "Peacock"}, _
-                                New Person With {.IsChecked = True, .FirstName = "Steven", .LastName = "Fuller"} _
-                            } _
-                        } _
-                    } _
-                }, _
-                New Person With { _
-                    .IsChecked = False, .FirstName = "Edwin", .LastName = "Thone", .Children = New List(Of Person) From { _
-                        New Person With {.IsChecked = False, .FirstName = "Steven", .LastName = "King"} _
-                    } _
-                } _
-            }
-        End Sub
-    End Class
+		Public Sub New()
+			Persons = New List(Of Person) _
+				From {
+					New Person With {.IsChecked = True, .FirstName = "Billy", .LastName = "Kelm", .Children = New List(Of Person) _
+						From {
+							New Person With {
+								.IsChecked = True, .FirstName = "Daniel", .LastName = "Earwood", .Children = New List(Of Person) From {
+									New Person With {
+										.IsChecked = True,
+										.FirstName = "Anne",
+										.LastName = "Peacock"
+									},
+									New Person With {
+										.IsChecked = True,
+										.FirstName = "Steven",
+										.LastName = "Fuller"
+									}
+								}
+							}
+						}
+						},
+					New Person With {
+						.IsChecked = False,
+						.FirstName = "Edwin",
+						.LastName = "Thone",
+						.Children = New List(Of Person) From {
+							New Person With {
+								.IsChecked = False,
+								.FirstName = "Steven",
+								.LastName = "King"
+							}
+						}
+					}
+				}
+		End Sub
+	End Class
 End Namespace
--- VB/TreeListCheckBoxes/Person.vb
+++ VB/TreeListCheckBoxes/Person.vb
@@ -4,13 +4,13 @@ Imports System.Linq
 Imports System.ComponentModel
 
 Namespace TreeListCheckBoxes
-    Public Class Person
-        Public Property IsChecked() As Boolean
-        Public Property FirstName() As String
-        Public Property LastName() As String
-        Public Property Children() As List(Of Person)
-        Public Sub New()
-            Children = New List(Of Person)()
-        End Sub
-    End Class
+	Public Class Person
+		Public Property IsChecked() As Boolean
+		Public Property FirstName() As String
+		Public Property LastName() As String
+		Public Property Children() As List(Of Person)
+		Public Sub New()
+			Children = New List(Of Person)()
+		End Sub
+	End Class
 End Namespace
