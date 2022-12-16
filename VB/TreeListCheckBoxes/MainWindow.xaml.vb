f377a4c43df20e24472228ef1a6dd26c693e1fc1
DevExpressExampleBot <fakeEmail>
2022-12-17 01:40:52 +0400

    Source auto update [skip ci]

--- VB/TreeListCheckBoxes/MainWindow.xaml.vb
+++ VB/TreeListCheckBoxes/MainWindow.xaml.vb
@@ -1,56 +1,22 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows
-Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Editors
 
 Namespace TreeListCheckBoxes
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
 
-	Public Class ViewModel
-		Public Property Persons() As List(Of Person)
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-		Public Sub New()
-			Persons = New List(Of Person) _
-				From {
-					New Person With {.IsChecked = True, .FirstName = "Billy", .LastName = "Kelm", .Children = New List(Of Person) _
-						From {
-							New Person With {
-								.IsChecked = True, .FirstName = "Daniel", .LastName = "Earwood", .Children = New List(Of Person) From {
-									New Person With {
-										.IsChecked = True,
-										.FirstName = "Anne",
-										.LastName = "Peacock"
-									},
-									New Person With {
-										.IsChecked = True,
-										.FirstName = "Steven",
-										.LastName = "Fuller"
-									}
-								}
-							}
-						}
-						},
-					New Person With {
-						.IsChecked = False,
-						.FirstName = "Edwin",
-						.LastName = "Thone",
-						.Children = New List(Of Person) From {
-							New Person With {
-								.IsChecked = False,
-								.FirstName = "Steven",
-								.LastName = "King"
-							}
-						}
-					}
-				}
-		End Sub
-	End Class
+    Public Class ViewModel
+
+        Public Property Persons As List(Of Person)
+
+        Public Sub New()
+            Persons = New List(Of Person) From {New Person With {.IsChecked = True, .FirstName = "Billy", .LastName = "Kelm", .Children = New List(Of Person) From {New Person With {.IsChecked = True, .FirstName = "Daniel", .LastName = "Earwood", .Children = New List(Of Person) From {New Person With {.IsChecked = True, .FirstName = "Anne", .LastName = "Peacock"}, New Person With {.IsChecked = True, .FirstName = "Steven", .LastName = "Fuller"}}}}}, New Person With {.IsChecked = False, .FirstName = "Edwin", .LastName = "Thone", .Children = New List(Of Person) From {New Person With {.IsChecked = False, .FirstName = "Steven", .LastName = "King"}}}}
+        End Sub
+    End Class
 End Namespace
